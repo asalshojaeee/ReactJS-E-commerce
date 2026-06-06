fe39556dc31ec8018ea20c37020ce649b816a504
Asal Shojaee <asal.shojaee.15@gmail.com>
2026-06-06 01:01:20 -0700

    style(ProductDetils):add css

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from 'react-router-dom'
 
 import axios from "axios"
 
 const ProductDetails = () => {
+    const [loading, setLoading] = useState(true)
+    const productImageListLoading = new Array(4).fill(null);
 
     const [data, setData] = useState({
         productName: "",
@@ -19,18 +21,64 @@ const ProductDetails = () => {
     const params = useParams()
 
     const fetchProductDetail = async () => {
+        setLoading(true)
         const response = await axios.post('http://localhost:3000/api/product-details', {
-            productId=params?.id,
+            productId: params?.id
 
 
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
+        }
 
-            }
-        })
-
+        )
+        // setLoading(false)
+        setData(response.data)
     }
+
+    useEffect(() => {
+        fetchProductDetail()
+    }, [])
+
+
+    return (
+
+
+        <div className="containar p-4 mx-auto">
+            <div className=" min-h-[200px]">
+
+                <div>
+                    <div className="h-96">
+                        {
+                            loading ? (
+                                <div className="flex gap-2 lg:flex-col overflow-scroll scrollbar-none h-full">
+                                    {
+                                        productImageListLoading.map((el, index) => {
+                                            return (
+                                                <div className="h-20 w-20 bg-slate-200 rounded"></div>
+                                            )
+
+                                        })
+                                    }
+                                </div>
+
+                            ) :
+                                (
+                                    <div></div>
+                                )
+                        }
+                    </div>
+                </div>
+
+
+
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+
+            </div>
+        </div>
+    )
 
 
 }
