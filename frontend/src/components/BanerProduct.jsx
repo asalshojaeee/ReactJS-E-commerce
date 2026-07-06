ae70af3ece3f1110c64f2a68fd5fdcd61fed89b2
Asal Shojaee <asal.shojaee.15@gmail.com>
2026-07-06 13:13:15 +0330

    change baner

--- frontend/src/components/BanerProduct.jsx
+++ frontend/src/components/BanerProduct.jsx
@@ -1,18 +1,18 @@
 import imageMobile from '../assests/banner/img1_mobile.jpg'
-import image2 from '../assests/banner/img2.webp'
-import image3 from '../assests/banner/img3.jpg'
-import image4 from '../assests/banner/img4.jpg'
-import image5 from '../assests/banner/img5.webp'
+import img24 from '../assests/banner/img24.jpg'
+import img21 from '../assests/banner/img21.jpg'
+import img20 from '../assests/banner/img20.jpg'
+import img19 from '../assests/banner/img19.jpg'
 
 import image2Mobile from '../assests/banner/img2_mobile.webp'
-import image3Mobile from '../assests/banner/img3_mobile.jpg'
-import image4Mobile from '../assests/banner/img4_mobile.jpg'
+import imgm23 from '../assests/banner/imgm23.jfif'
+import imgm22 from '../assests/banner/imgm22.jfif'
 
-import image5Mobile from '../assests/banner/img5_mobile.png'
+import imgm20 from '../assests/banner/imgm20.jpg'
 
 
 
-import image1 from '../assests/banner/img1.webp'
+import img23 from '../assests/banner/img23.jpg'
 import { useEffect, useState } from 'react'
 import { FaAngleRight } from 'react-icons/fa6'
 import { FaAngleLeft } from 'react-icons/fa6'
@@ -20,11 +20,11 @@ import { FaAngleLeft } from 'react-icons/fa6'
 const BanerProduct = () => {
     const [currentImage, setCurrentImage] = useState(0)
     const desktopImages = [
-        image1, image2, image3, image4, image5
+        img19, img20, img21, img23, img24
     ]
     const mobileImages = [
-        imageMobile,
-        image2Mobile, image3Mobile, image4Mobile, image5Mobile
+        imgm20,
+        imgm22, imgm23, imgm22, imgm20
     ]
     const nextImage = () => {
         if (desktopImages.length - 1 > currentImage) {
